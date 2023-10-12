7ccfa85413a4f5b0e2e9b9777e8d5e328d018bd0
Dmitrii <rakhmanin.dm@gmail.com>
2023-10-12 21:03:13 +0300

    ref: move app to separate file, add config file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,6 @@
-import express from 'express';
+import app from './app';
+import { PORT } from './config';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
-
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+app.listen(PORT, () => {
+  console.log(`[ ready ] http://localhost:${PORT}`);
 });
