984b55f9619f44b119d65c5545ee2fc93d44eb69
Dmitrii <rakhmanin.dm@gmail.com>
2023-10-15 13:44:04 +0300

    feat: add form provider

--- apps/front/src/components/Form/Form.tsx
+++ apps/front/src/components/Form/Form.tsx
@@ -1,16 +1,36 @@
 import { FC } from 'react';
 import block from 'bem-css-modules';
 import cn from 'classnames';
+import { FormProvider, useForm, SubmitHandler } from 'react-hook-form';
 import styles from './Form.module.css';
 
 const b = block(styles);
 
+interface Inputs {
+  email: string;
+  number?: string;
+}
+
 const Form: FC<{ className?: string }> = ({ className = undefined }) => {
+  const methods = useForm<Inputs>({
+    mode: 'onTouched',
+    defaultValues: {
+      email: '',
+      number: '',
+    },
+  });
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
-    <form className={cn(b(), className)}>
-      <input placeholder="email" />
-      <input placeholder="number" />
-    </form>
+    <FormProvider {...methods}>
+      <form
+        className={cn(b(), className)}
+        onSubmit={methods.handleSubmit(onSubmit)}
+      >
+        <input className={b('input')} placeholder="email" />
+        <input className={b('input')} placeholder="number" />
+        <button type="submit">Search</button>
+      </form>
+    </FormProvider>
   );
 };
 
