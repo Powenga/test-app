c91c8de09e7bd6330ad08f52852e25b686856e6d
Dmitrii <rakhmanin.dm@gmail.com>
2023-10-16 00:46:49 +0300

    ref: add common setup function to from validation tests, move render to beforeEach

--- apps/front/src/components/Form/Form.test.tsx
+++ apps/front/src/components/Form/Form.test.tsx
@@ -1,85 +1,87 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import Form from './Form';
 import '@testing-library/jest-dom';
 import { EMAIL_MAX, ValidationMessages } from '@test-app/validation';
 import { generateString } from '../../utils/test-utils';
 
 describe('Form', () => {
-  it('should render successfully', () => {
+  beforeEach(() => {
     render(<Form />);
+  });
+
+  const setup = () => {
+    const emailInput = screen.getByLabelText('email');
+    const numberInput = screen.getByLabelText('number');
+    return { emailInput, numberInput };
+  };
+
+  it('should be rendering successfully', () => {
     expect(screen.getByRole('form')).toBeInTheDocument();
     expect(screen.getByLabelText('email')).toBeInTheDocument();
     expect(screen.getByLabelText('number')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Search' })).toBeInTheDocument();
   });
   describe('email validation', () => {
-    const setup = () => {
-      render(<Form />);
-      const initError = screen.queryByRole('alert');
-      const nameInput = screen.getByLabelText('email');
-      return { nameInput, initError };
-    };
     it('should show required error', async () => {
-      const { nameInput, initError } = setup();
-      expect(initError).not.toBeInTheDocument();
-      fireEvent.focus(nameInput);
-      fireEvent.blur(nameInput);
+      const { emailInput } = setup();
+      expect(screen.queryByRole('alert')).not.toBeInTheDocument();
+      fireEvent.focus(emailInput);
+      fireEvent.blur(emailInput);
       expect(await screen.findByRole('alert')).toHaveTextContent(
         ValidationMessages.emailRequired
       );
     });
     it('should show invalid error', async () => {
-      const { nameInput, initError } = setup();
-      expect(initError).not.toBeInTheDocument();
-      fireEvent.change(nameInput, { target: { value: 'invalid-email' } });
-      fireEvent.blur(nameInput);
+      const { emailInput } = setup();
+      expect(await screen.queryByRole('alert')).not.toBeInTheDocument();
+      expect(screen.queryByRole('alert')).not.toBeInTheDocument();
+      fireEvent.change(emailInput, { target: { value: 'invalid-email' } });
+      fireEvent.blur(emailInput);
       expect(await screen.findByRole('alert')).toHaveTextContent(
         ValidationMessages.invalidEmail
       );
     });
     it('should show max error', async () => {
-      const { nameInput, initError } = setup();
-      expect(initError).not.toBeInTheDocument();
-      fireEvent.change(nameInput, {
+      const { emailInput } = setup();
+      expect(screen.queryByRole('alert')).not.toBeInTheDocument();
+      fireEvent.change(emailInput, {
         target: {
           value: generateString({ length: EMAIL_MAX + 1, isEmail: true }),
         },
       });
-      fireEvent.blur(nameInput);
+      fireEvent.blur(emailInput);
       expect(await screen.findByRole('alert')).toHaveTextContent(
         ValidationMessages.emailMax
       );
     });
   });
   describe('number field', () => {
-    const setup = () => {
-      render(<Form />);
-      const initError = screen.queryByRole('alert');
-      const input = screen.getByLabelText('number');
-      return { input, initError };
-    };
     it('should show invalid error', async () => {
-      const { input, initError } = setup();
-      expect(initError).not.toBeInTheDocument();
-      fireEvent.change(input, { target: { value: '1111' } });
-      fireEvent.blur(input);
+      const { numberInput } = setup();
+      expect(screen.queryByRole('alert')).not.toBeInTheDocument();
+      fireEvent.change(numberInput, { target: { value: '1111' } });
+      fireEvent.blur(numberInput);
       expect(await screen.findByRole('alert')).toHaveTextContent(
         ValidationMessages.invalidNumber
       );
     });
     it('should add dashes', async () => {
-      const { input, initError } = setup();
-      expect(initError).not.toBeInTheDocument();
-      fireEvent.change(input, { target: { value: '112233' } });
-      fireEvent.blur(input);
-      expect(input).toHaveValue('11-22-33');
+      const { numberInput } = setup();
+      expect(screen.queryByRole('alert')).not.toBeInTheDocument();
+      fireEvent.change(numberInput, { target: { value: '112233' } });
+      fireEvent.blur(numberInput);
+      await waitFor(() => {
+        expect(numberInput).toHaveValue('11-22-33');
+      });
     });
     it('should remove extra characters', async () => {
-      const { input, initError } = setup();
-      expect(initError).not.toBeInTheDocument();
-      fireEvent.change(input, { target: { value: '11223344' } });
-      fireEvent.blur(input);
-      expect(input).toHaveValue('11-22-33');
+      const { numberInput } = setup();
+      expect(screen.queryByRole('alert')).not.toBeInTheDocument();
+      fireEvent.change(numberInput, { target: { value: '11223344' } });
+      fireEvent.blur(numberInput);
+      await waitFor(() => {
+        expect(numberInput).toHaveValue('11-22-33');
+      });
     });
   });
 });
