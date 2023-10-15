7e18fb152728efb4ca5dae25f9f4fc371f3509c5
Dmitrii <rakhmanin.dm@gmail.com>
2023-10-15 15:10:01 +0300

    feat: add cors middleware to api

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,9 +1,12 @@
 import express, { json } from 'express';
 import router from './routes';
+import cors from 'cors';
 import { errorHandler } from './middlewares/error-handler';
+import { ORIGIN } from './config';
 
 const app = express();
 
+app.use(cors({ origin: ORIGIN.split(' ') }));
 app.use(json());
 app.use(router);
 
--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -2,4 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const { PORT = 3000, DELAY = 5000 } = process.env;
+export const {
+  PORT = 3000,
+  DELAY = 5000,
+  ORIGIN = 'http://localhost:4200',
+} = process.env;
