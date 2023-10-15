fb84bda035d62b63a2efd8143849c9b607baf1e7
Dmitrii <rakhmanin.dm@gmail.com>
2023-10-15 19:39:26 +0300

    ref: change email validation to regex variant

--- libs/validation/src/lib/validation.ts
+++ libs/validation/src/lib/validation.ts
@@ -4,10 +4,15 @@ import { ValidationMessages } from '../constants';
 export const EMAIL_MAX = 50;
 const numberRegex = /^\d{6}$/;
 const numberFrontRegex = /^\d{2}-\d{2}-\d{2}$/;
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const emailSchema = yup
   .string()
-  .email(ValidationMessages.invalidEmail)
+  .matches(emailRegex, {
+    message: ValidationMessages.invalidEmail,
+    excludeEmptyString: true,
+  })
   .required(ValidationMessages.emailRequired)
   .typeError(ValidationMessages.stringRequired)
   .max(EMAIL_MAX, ValidationMessages.emailMax);
