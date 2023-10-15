3667366ffe2f751c667d65022ef57430f613eadd
Dmitrii <rakhmanin.dm@gmail.com>
2023-10-15 12:27:17 +0300

    feat: add remove unknown fields test

--- apps/api/src/controllers/user.test.ts
+++ apps/api/src/controllers/user.test.ts
@@ -5,8 +5,48 @@ import { ErrorMessages } from '../utils/constants';
 
 const request = supertest(app);
 
+const checkFindUserValidation = jest.fn();
+const checkFindUser = jest.fn();
+
 jest.mock('./users.json', () => {
   return JSON.stringify(TEST_USERS);
+});
+
+jest.mock('../middlewares/validator', () => {
+  const originalModules = jest.requireActual('../middlewares/validator');
+  return {
+    __esModule: true,
+    ...originalModules,
+    validateFindRequest: (req, res, next) => {
+      checkFindUserValidation(req.body);
+      return originalModules.validateFindRequest(req, res, next);
+    },
+  };
+});
+
+jest.mock('./user', () => {
+  const originalModules = jest.requireActual('./user');
+  return {
+    __esModule: true,
+    ...originalModules,
+    getUsers: (req, res, next) => {
+      checkFindUser(req.body);
+      return originalModules.getUsers(req, res, next);
+    },
+  };
+});
+
+jest.mock('../middlewares/delay', () => {
+  const originalModules = jest.requireActual('../middlewares/delay');
+  return {
+    __esModule: true,
+    ...originalModules,
+    delay: (req, res, next) => next(),
+  };
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
 });
 
 describe('Find user request should', () => {
@@ -19,5 +59,21 @@ describe('Find user request should', () => {
     const response = await request.post('/').send({ email: 'some@email.host' });
     expect(response.status).toBe(404);
     expect(response.body.message).toBe(ErrorMessages.usersNotFound);
+  });
+});
+
+describe('Find user request validation', () => {
+  it('should remove unexpected fields', async () => {
+    const body = {
+      email: 'some@email.com',
+      unexpected: 'unexpected',
+    };
+    await request.post('/').send(body);
+    expect(checkFindUserValidation).toBeCalledWith(
+      expect.objectContaining(body)
+    );
+    expect(checkFindUser).toBeCalledWith(
+      expect.not.objectContaining({ unxected: 'unexpected' })
+    );
   });
 });
